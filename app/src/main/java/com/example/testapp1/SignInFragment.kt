66868f5cb338228e5ea06fddbbeee6c8ea1cc30f
alexey-chaichuk <alexey.chaichuk@gmail.com>
2022-04-06 16:15:43 +0500

    completed

---
+++ app/src/main/java/com/example/testapp1/SignInFragment.kt
@@ -0,0 +1,36 @@
+package com.example.testapp1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+
+class SignInFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val goToSignUp: TextView = view.findViewById(R.id.goto_signup)
+        goToSignUp.setOnClickListener {
+            findNavController().navigate(
+                SignInFragmentDirections.actionSignInFragmentToSignUpFragment())
+        }
+
+        val backToSignUp: ImageView = view.findViewById(R.id.back_to_signup)
+        backToSignUp.setOnClickListener {
+            findNavController().navigate(
+                SignInFragmentDirections.actionSignInFragmentToSignUpFragment())
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+}
