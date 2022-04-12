251d8019f7cb8d1f7d89d76425f067f3392c3b0f
alexey-chaichuk <alexey.chaichuk@gmail.com>
2022-04-12 16:59:47 +0500

    moving menu drawer to main activity

--- app/src/main/java/com/stocksignalplusapp/us/MainActivity.kt
+++ app/src/main/java/com/stocksignalplusapp/us/MainActivity.kt
@@ -1,9 +1,40 @@
 package com.stocksignalplusapp.us
 
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.stocksignalplusapp.us.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity_main), MenuDrawer  {
 
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
+    override fun onResume() {
+        super.onResume()
+        binding.menuNavigation.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            binding.drawerLayout.close()
+            true
+        }
+    }
+
+    override fun openMenu() {
+        binding.drawerLayout.open()
+    }
+
+    override fun closeMenu() {
+        binding.drawerLayout.close()
+    }
+
+}
+
+interface MenuDrawer {
+    fun openMenu()
+    fun closeMenu()
 }
