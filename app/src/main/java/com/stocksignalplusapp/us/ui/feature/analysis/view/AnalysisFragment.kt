f67c7d7e4e878e7cdb7521fd7e205ac26ff1a275
alexey-chaichuk <alexey.chaichuk@gmail.com>
2022-04-17 04:26:07 +0500

    start adding analysis fragment

---
+++ app/src/main/java/com/stocksignalplusapp/us/ui/feature/analysis/view/AnalysisFragment.kt
@@ -0,0 +1,8 @@
+package com.stocksignalplusapp.us.ui.feature.analysis.view
+
+import androidx.fragment.app.Fragment
+import com.stocksignalplusapp.us.R
+
+class AnalysisFragment : Fragment(R.layout.fragment_analysis) {
+
+}
